e627859d94464d7bdf43703e591a406f98c2c70c
Ferdinand <atiferdinand31@gmail.com>
2026-05-07 12:05:46 +0100

    fix(confi.h) : Correction des multiples définitions des variables présents dans le config.h

--- config/Config.h
+++ config/Config.h
@@ -4,10 +4,10 @@
 
 namespace dsConfig {
     constexpr size_t MAX_FILENAME_LEN = 256;
-    std::string refresh_token;
-    std::string client_secret;
-    std::string client_id;
-    std::string access_token;
+    inline std::string refresh_token;
+    inline  std::string client_secret;
+    inline std::string client_id;
+    inline std::string access_token;
 }
 
 #endif
